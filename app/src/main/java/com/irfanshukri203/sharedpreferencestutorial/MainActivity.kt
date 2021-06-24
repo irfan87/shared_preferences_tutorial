c3b9840bb9d51204e9d3ba306a5475d28e4006a7
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-24 23:25:17 +0800

    make a dummy form to play around with the shared preferences

--- app/src/main/java/com/irfanshukri203/sharedpreferencestutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/sharedpreferencestutorial/MainActivity.kt
@@ -2,10 +2,40 @@ package com.irfanshukri203.sharedpreferencestutorial
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("userPref", MODE_PRIVATE)
+        val sharedPrefEditor = sharedPref.edit()
+
+        // writing the data
+        btnSave.setOnClickListener {
+            val name = etName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val isAdult = cbAdult.isChecked
+
+            sharedPrefEditor.apply {
+                putString("name", name)
+                putInt("age", age)
+                putBoolean("isAdult", isAdult)
+
+                apply()
+            }
+        }
+
+        // load / read the data
+        btnLoad.setOnClickListener {
+            val name = sharedPref.getString("name", null)
+            val age = sharedPref.getInt("age", 0)
+            val isAdult = sharedPref.getBoolean("isAdult", false)
+
+            etName.setText(name)
+            etAge.setText(age.toString())
+            cbAdult.isChecked = isAdult
+        }
     }
 }
